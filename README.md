7c7cc97ae609c77a354eca66d5722f3e33498c2c
Gabriel Ogbonnaya <ogbonnaya83512@gmail.com>
2020-12-21 23:26:10 -0700

    added second part of project and updated readme

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 This is a facial recognition project
 Description:
+This project is broken up into two parts.
+
+Part 1 is a function (project_1_facial_recognition) that processes two images and acertains if the same face appears in both pictures and outputs a number representation of the certainty thereof onto the test image. See image below:
+
+Part 2 is a web cam facial recognition fucnction (project_2_facial_recognition_attendance) which based on known faces stored in the face_2 directory can classify faces introduced in the web cam frame. see imgaes below:
+
+    Limitations:
+    Varying facial expressions and imgas with differing angles increase the inconsitency of results, also processing speed is slow with python, C++ would be better suited for intensive calculations
+
+Goals
+Further understanding of opencv techniques as well as get an introduction to computervision facial recognition algorithms
+
+Further Applications:
+Facial Recognition can be used in a variety of instances from security, to search and rescue, person tracking.
+
 Background https://medium.com/@ageitgey/machine-learning-is-fun-part-4-modern-face-recognition-with-deep-learning-c3cffc121d78
 
 Youtube tutorial:
@@ -8,6 +23,8 @@ To Run:
 pipenv run python basics.py
 
 To Install:
+clone the directory
+
 **_ assuming pip env has not on system _**
 refer to the following links for the installation of pipenv
 https://packaging.python.org/tutorials/managing-dependencies/
@@ -16,6 +33,24 @@ https://packaging.python.org/tutorials/managing-dependencies/
 
     pipenv install
     pipenv shell
+
+You will also need to add vs code where to find your virtualenv
+
+First, navigate to your Pipenv directory (where your Pipfile is located). Then run pipenv --venv, to get the full path to the Pipenv's virtualenv. This is probably something like /home/myuser/.local/share/virtualenvs/projectname, depending on which operating system you are on.
+
+Next, press CTRL+Shift+P (Cmd in MacOS) and search for "Select Workspace Interpreter". Press enter, and select any of the suggested interpreters. Upon doing this, a .vscode directory will be created inside your project root, containing a settings.json. Open this file, and set python.pythonPath to the virtualenv path you found earlier, and add /bin/python at the end.
+
+In other words, settings.json should look something like this:
+
+{
+"python.pythonPath": "/home/myuser/.local/share/virtualenvs/projectname/bin/python"
+}
+
+or
+
+click in the bottom left area of the terminal as seen in the image
+
+Save the settings file, and restart Visual Studio Code, for the changes to take effect.
 
 Notes
 To install a requiments.txt file run:
@@ -31,3 +66,6 @@ pipenv install -r path/to/requirements.txt
 
     To deactivate shell run:
         deactivate
+
+Extra: Readme Cheat sheet
+https://levelup.gitconnected.com/github-readme-cheatsheet-617dff61fa23
--- basics.py
+++ basics.py
@@ -1,6 +1,7 @@
 import numpy as np
 import face_recognition
 import cv2
+import os
 
 
 def resize(img, scale_percent_in=1):
@@ -11,7 +12,8 @@ def resize(img, scale_percent_in=1):
     return cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
 
 
-def facial_recognition():
+def project_1_facial_recognition():
+    """This function processes two images and acertains if the same face appears in both"""
     imgElon = face_recognition.load_image_file('faces/elon_musk_1.jpg')
     imgElon = cv2.cvtColor(imgElon, cv2.COLOR_BGR2RGB)
     imgElon = resize(imgElon, scale_percent_in=40)
@@ -31,10 +33,76 @@ def facial_recognition():
     encodeElonTest = face_recognition.face_encodings(imgElonTest)[0]
     cv2.rectangle(imgElonTest, (faceLocTest[3], faceLocTest[0]),
                   (faceLocTest[1], faceLocTest[2]), (255, 0, 255), 2)
+
+    results = face_recognition.compare_faces([encodeElon], encodeElonTest)
+    # lower the distance the better the match
+    faceDis = face_recognition.face_distance([encodeElon], encodeElonTest)
+    print(results, faceDis)  # values range from zero to 1
+
+    cv2.putText(imgElonTest, f'{results} {round(faceDis[0],2)}', (
+        50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
+
     cv2.imshow("Elon Musk", imgElon)
     cv2.imshow("Elon Musk Test", imgElonTest)
     cv2.waitKey(0)
 
 
+def findEncodings(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+
+def project_2_facial_recognition_attendance():
+    path = 'faces_2'
+    images = []
+    classNames = []
+    myList = os.listdir(path)
+    for cls in myList:
+        curImg = cv2.imread(f'{path}/{cls}')
+        images.append(curImg)
+        classNames.append(os.path.splitext(cls)[0])
+
+    encodeListKnown = findEncodings(images)
+
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        success, img = cap.read()
+        imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+        imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+        faceCurFrame = face_recognition.face_locations(imgS)
+        encodesCurFrame = face_recognition.face_encodings(imgS, faceCurFrame)
+
+        for encodeFace, faceLoc in zip(encodesCurFrame, faceCurFrame):
+            matches = face_recognition.compare_faces(
+                encodeListKnown, encodeFace)
+            faceDis = face_recognition.face_distance(
+                encodeListKnown, encodeFace)
+
+            matchIndex = np.argmin(faceDis)
+
+            if matches[matchIndex]:
+                name = classNames[matchIndex].upper()
+
+                y1, x2, y2, x1 = faceLoc
+                y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
+                cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                cv2.rectangle(img, (x1, y2+35), (x2, y2),
+                              (0, 255, 0), cv2.FILLED)
+
+                cv2.putText(img, name, (x1+6, y2-6),
+                            cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+            cv2.imshow('Webcam', img)
+            cv2.waitKey(1)
+
+
 if __name__ == '__main__':
-    facial_recognition()
+
+    # project_1_facial_recognition()
+
+    project_2_facial_recognition_attendance()
